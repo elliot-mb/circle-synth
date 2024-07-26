9524ae37c022f0d703bc60a9453b59a862dd4714
elliot-mb <elliotbuckingham14@gmail.com>
2024-07-26 19:17:14 +0100

    notes as constants

--- src/lib/audio/constants.ts
+++ src/lib/audio/constants.ts
@@ -11,3 +11,7 @@ export const CHAR_SIZE = 8;
 export const WAV_HEADER_CHUNK = 16;
 export const BYTES_IN_INT = 4;
 export const BYTES_IN_16b = 2;
+export const SEMITONE_SPACING = Math.pow(2, 1 / 12);
+export const NOTES = Array.from(Array(9).keys()).map((x, i) => 55 * Math.pow(2, i)); //frequencies of A_1 to A_9
+export const ALL_NOTES = NOTES.map(f => Array.from(Array(12).keys()).map((x, i) => f * Math.pow(SEMITONE_SPACING, i)));
+console.log(ALL_NOTES);
