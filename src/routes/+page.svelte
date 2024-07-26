ec0ac52c934f730652960f8f93bec8b461badd02
elliot-mb <elliotbuckingham14@gmail.com>
2024-07-26 16:49:30 +0100

    downloader_button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,17 @@
 <script lang='ts'>
     import {SAMPLE_RATE, A_4} from "$lib/audio/constants";
+    import { bufferToWav } from "$lib/audio/converter";
     import { square, squareLerpPW, squarePulseWidth } from "$lib/audio/oscillator";
     import { onMount } from 'svelte';
 
+    const seconds: number = 1;
     let arrayBuffer: AudioBuffer;
     let arrayBuffer2: AudioBuffer;
     let audioCtx: AudioContext;
     let source: AudioBufferSourceNode;
+    let blobLink: string = 'none';
+    let downloader: HTMLAnchorElement;
+    let canDownload: boolean = false;
 
     let s: number = 0;
 
@@ -14,7 +19,6 @@
         const AudioContext = window.AudioContext;
 
         audioCtx = new AudioContext({sampleRate: SAMPLE_RATE});
-        const seconds = 1;
         arrayBuffer = audioCtx.createBuffer(
             1, //1 is mono
             audioCtx.sampleRate * seconds,
@@ -35,6 +39,13 @@
 
         s = audioCtx.sampleRate;
         source = audioCtx.createBufferSource();
+
+        const maybeDownloader = document.getElementById('downloader');
+        if(maybeDownloader !== null) {
+            downloader = <HTMLAnchorElement> maybeDownloader;
+            canDownload = true;
+        } 
+
     });
 
     const handleButtonPress = () => {
@@ -50,6 +61,15 @@
         source.connect(audioCtx.destination);
         source.start();
     }
+
+    const saveWav = () => {
+        const wav: Blob = bufferToWav(arrayBuffer, arrayBuffer.sampleRate * seconds);
+        const url = window.URL.createObjectURL(wav);
+        downloader.href = url;
+        downloader.download = 'file.wav';
+        downloader.click();
+        window.URL.revokeObjectURL(url);
+    }
 </script>
 
 <svelte:window/>
@@ -57,3 +77,5 @@
 <h2>Here is a button to make some noise! @ {s}Hz sample rate</h2>
 <button type='button' on:click={handleButtonPress}>noise</button>
 <button type='button' on:click={handlePWPress}>noise2</button>
+<button type='button' style='display: {canDownload ? 'block' : 'none'};' on:click={saveWav}>save wav</button>
+<a id='downloader' href={blobLink} style='display: none'>save wav</a>
