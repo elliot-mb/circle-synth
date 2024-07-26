54cb9e1d1602fe093b3b7c4da418fa7ae8b29ac2
elliot-mb <elliotbuckingham14@gmail.com>
2024-07-26 17:08:29 +0100

    wav rendering working git push

--- src/lib/audio/converter.ts
+++ src/lib/audio/converter.ts
@@ -1,6 +1,6 @@
 // conversion-to-wav related functions go here 
 
-import { CHAR_SIZE, NIBB_SIZE, NUM_CHANS, WAV_HEADER_CHUNK, WAV_HEADER_DATA, WAV_HEADER_FMT, WAV_HEADER_RIFF, WAV_HEADER_WAVE, WAV_HEADERSIZE } from "$lib/audio/constants";
+import { CHAR_SIZE, NIBB_SIZE, NUM_OUT_CHANS, WAV_HEADER_CHUNK, WAV_HEADER_DATA, WAV_HEADER_FMT, WAV_HEADER_RIFF, WAV_HEADER_WAVE, WAV_HEADERSIZE } from "$lib/audio/constants";
 
 /**
  * 
@@ -9,7 +9,7 @@ import { CHAR_SIZE, NIBB_SIZE, NUM_CHANS, WAV_HEADER_CHUNK, WAV_HEADER_DATA, WAV
  * @returns blob of type wav
  */
 export const bufferToWav = (soundBuff: AudioBuffer, len: number): Blob => {
-    const fileSize: number = len * NUM_CHANS * 2 + WAV_HEADERSIZE;
+    const fileSize: number = len * NUM_OUT_CHANS * 2 + WAV_HEADERSIZE;
     const file: ArrayBuffer = new ArrayBuffer(fileSize);
     const view: DataView = new DataView(file);
     
@@ -26,16 +26,16 @@ export const bufferToWav = (soundBuff: AudioBuffer, len: number): Blob => {
     }
 
     write32b(WAV_HEADER_RIFF);
-    write32b(length - CHAR_SIZE);
+    write32b(fileSize - CHAR_SIZE);
     write32b(WAV_HEADER_WAVE);
     
     write32b(WAV_HEADER_FMT);
     write32b(WAV_HEADER_CHUNK);
     write16b(1);
-    write16b(NUM_CHANS);
+    write16b(NUM_OUT_CHANS);
     write32b(soundBuff.sampleRate);
-    write32b(soundBuff.sampleRate * 2 * NUM_CHANS);
-    write16b(NUM_CHANS * 2);
+    write32b(soundBuff.sampleRate * 2 * NUM_OUT_CHANS);
+    write16b(NUM_OUT_CHANS * 2);
     write16b(WAV_HEADER_CHUNK);
 
     write32b(WAV_HEADER_DATA);
@@ -44,9 +44,12 @@ export const bufferToWav = (soundBuff: AudioBuffer, len: number): Blob => {
     const wavData: Float32Array = soundBuff.getChannelData(0);
 
     while(pos < fileSize){
-        const sample = wavData[offset];
-        const scaled = (0.5 + sample < 0 ? sample * 32768 : sample * 32767)|0; // |0 truncates into an integer by bitwise or 
-        write16b(scaled);
+        for(let i = 0; i < NUM_OUT_CHANS; i++){
+            const sample = wavData[offset];
+            const scaled = (0.5 + sample < 0 ? sample * 32768 : sample * 32767)|0; // |0 truncates into an integer by bitwise or 
+            view.setInt16(pos, scaled, true);
+            pos += 2;
+        }
         offset++;
     }
 
