39a6a2a814b9781ea1db7671a46b24c9a7625697
elliot-mb <elliotbuckingham14@gmail.com>
2024-07-26 18:58:38 +0100

    note selection

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -64,16 +64,45 @@
     <InfoGrid>
         <p>Sample Rate</p>
         <p>{sampleRate}Hz</p>
-        <p>Current Note</p>
-        <p>{sampleRate}Hz</p>
+        <InfoGrid columns={1}>
+            <p>Current Note</p>
+            <button class='btn-primary' type='button' on:click={handleButtonPress}>Play</button>
+        </InfoGrid>
+        <div class="dropdown">
+            <select name="notes">
+                <option value={440}>A-3(220)</option>
+                <option value={440}>A-4(440)</option>
+                <option value={440}>A-5(880)</option>
+            </select>
+        </div>
     </InfoGrid>
-    <InfoGrid>
-        <p>Waveform</p>
-        <p>wiggle wiggle</p>
-    </InfoGrid>
+    <p>Waveform</p>
     <p>Sine Terms</p>
-    <p>list</p>
+    <InfoGrid columns={3}>
+        <InfoGrid columns={1}>
+            <p>frequency</p>
+            <p>2000</p>
+        </InfoGrid>
+        <p>offset</p>
+        <p>amplitude</p>
+        <InfoGrid columns={1}>
+            <p>frequency</p>
+            <p>2000</p>
+        </InfoGrid>
+        <p>offset</p>
+        <p>amplitude</p>
+    </InfoGrid>
+    <InfoGrid columns={3}>
+        <p>frequency</p>
+        <p>offset</p>
+        <p>amplitude</p>
+        <p>frequency</p>
+        <p>offset</p>
+        <p>amplitude</p>
+    </InfoGrid>
+    <p>Square Terms</p>
 </InfoGrid>
-<button type='button' on:click={handleButtonPress}>noise</button>
+
+
 <button type='button' style='display: {canDownload ? 'block' : 'none'};' on:click={saveWav}>save wav</button>
 <a id='downloader' href={blobLink} style='display: none'>save wav</a>
